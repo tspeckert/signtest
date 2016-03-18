a1379db596b28cf2c5785ce3c84a150179966a4a
Thomas Speckert <tspeckert@gmail.com>
2016-03-18 14:31:37 +0100

    added key decrypting program

---
+++ PfxToSnk/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+namespace PfxToSnk
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            String snkFile = args[0];
+            String pfxFile = args[1];
+            String password = args[2];
+            Byte[] pfxBytes = File.ReadAllBytes(pfxFile);
+            var certificate = new X509Certificate2(pfxBytes, password, X509KeyStorageFlags.Exportable);
+            var privateKey = (RSACryptoServiceProvider)certificate.PrivateKey;
+            Byte[] snkBytes = privateKey.ExportCspBlob(true);
+            File.WriteAllBytes(snkFile, snkBytes);
+        }
+    }
+}
